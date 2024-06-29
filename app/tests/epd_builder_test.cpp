8dcfb07d38d3c7ef147ec0114f1da6fedaeb2f77
Gahtan Nahdi <155860115+gahtan-syarif@users.noreply.github.com>
2024-06-30 04:00:38 +0700

    Update epd_builder_test.cpp

--- app/tests/epd_builder_test.cpp
+++ app/tests/epd_builder_test.cpp
@@ -6,13 +6,6 @@ namespace fast_chess {
 TEST_SUITE("EPD Builder Tests") {
     TEST_CASE("EPD Creation White Start") {
         MatchData match_data;
-        match_data.players.first.config.name = "engine1";
-        match_data.players.first.color       = chess::Color::WHITE;
-        match_data.players.first.result      = chess::GameResult::WIN;
-
-        match_data.players.second.config.name = "engine2";
-        match_data.players.second.color       = chess::Color::BLACK;
-        match_data.players.second.result      = chess::GameResult::LOSE;
 
         match_data.moves = {MoveData("e2e4", "+1.00", 1321, 15, 4, 0, 0),
                             MoveData("e7e5", "+1.23", 430, 15, 3, 0, 0),
@@ -20,8 +13,6 @@ TEST_SUITE("EPD Builder Tests") {
                             MoveData("g8f6", "+10.15", 1821, 18, 7, 0, 0)};
 
         match_data.fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-
-        match_data.reason = "engine2 got checkmated";
 
         options::Tournament options;
 
@@ -33,21 +24,12 @@ TEST_SUITE("EPD Builder Tests") {
 
     TEST_CASE("EPD Creation Black Start") {
         MatchData match_data;
-        match_data.players.first.config.name = "engine1";
-        match_data.players.first.color       = chess::Color::BLACK;
-        match_data.players.first.result      = chess::GameResult::NONE;
-
-        match_data.players.second.config.name = "engine2";
-        match_data.players.second.color       = chess::Color::WHITE;
-        match_data.players.second.result      = chess::GameResult::NONE;
 
         match_data.moves = {MoveData("e8g8", "+1.00", 1321, 15, 4, 0, 0),
                             MoveData("e1g1", "+1.23", 430, 15, 3, 0, 0),
                             MoveData("a6c5", "+1.45", 310, 16, 24, 0, 0)};
 
         match_data.fen = "r2qk2r/1bpp2pp/n3pn2/p2P1p2/1bP5/2N1BNP1/1PQ1PPBP/R3K2R b KQkq - 0 1";
-
-        match_data.reason = "aborted";
 
         options::Tournament options;
 
